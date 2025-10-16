02438ac8bbc4fb2489487af177fd0bf8d86f6a28
Kpellehboy <elijahmflomo@gmail.com>
2025-10-16 18:32:07 +0530

    first commit

---
+++ stage0/app.py
@@ -0,0 +1,36 @@
+from flask import Flask, Response
+import requests
+import os
+import json
+from datetime import datetime, timezone
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+@app.route('/me', methods=['GET'])
+
+
+def get_profile(): # Fetch user details from environment variables
+    response = {
+        "status": "success",
+
+        "user": {
+            "email": os.getenv("USER_EMAIL", "your.elijahmflomo@gmail.com"),
+            "name": os.getenv("USER_NAME", "Elijah M. Flomo"),
+            "stack": os.getenv("USER_STACK", "Build a Dynamic Profile Endpoint with Flask")
+        },
+        
+        "timestamp": datetime.now(timezone.utc).isoformat(),
+        "fact": requests.get("https://catfact.ninja/fact").json().get("fact")
+    }
+    json_output = json.dumps(response, indent=2)
+
+    return Response(
+        json_output,
+        mimetype='application/json'
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
